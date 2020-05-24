dc5deacaa176e996ffbae67c1c7e191499cfce25
kn1618 <63390753+kn1618@users.noreply.github.com>
2020-05-24 22:40:48 +0900

    Initial commit

---
+++ contract/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const BlockChain_GateWay = require('./lib/gateway');
+
+module.exports.BlockChain_GateWay = BlockChain_GateWay;
+module.exports.contracts = [ BlockChain_GateWay ];
---
+++ web-app/src/app.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const ejs = require('ejs');
+const path = require('path');
+const viewspath = 'src/views'
+let network = require('./fabric/network.js');
+
+const app = express();
+
+app.engine('ejs', ejs.renderFile);
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+app.use(express.static('public'));
+
+// トップページ
+app.get("/", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'top.ejs'),
+        {title: 'Top Page',
+         link1:{href:'/request', text: 'Go to request page!'},
+         link2:{href:'/receipt', text: 'Go to receipt page!'}
+        });
+    });
+
+// リクエストページ
+app.get("/request", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'request.ejs'),
+        {title: 'Request Page',
+         content: 'Please input necessary information'
+        });
+    });
+
+// リクエスト完了ページ　*ブロックへの書き込み
+app.post("/request", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'request_completed.ejs'),
+        {title: 'Request Page',
+         content: 'Your Request has been sent',
+         link:{href:'/', text: 'Back to top page!'}
+        });
+    /* network.requestInfo(req.body.companyFrom, req.body.companyTo, req.body.dataAttribute, req.body.objectUsr, req.body.purpose, req.body.requestCount)
+    .then((response) => {
+        res.send(response);}); */
+    });
+
+// 承認ページ
+app.get("/approval", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'approval.ejs'),
+        {title: 'Approval Page'});
+    });
+
+// 承認完了ページ　*ブロックへの書き込み
+app.post("/approval", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'approval_completed.ejs'),
+        {content: 'Completed!'});
+    /* network.approval(req.body.name).then((response) => {
+        res.send(response);}); */
+    });
+    
+// 受領通知ページ
+app.get("/receipt", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'receipt.ejs'),
+        {title: 'Receipt Page',
+         content: 'If you have any message'
+        });
+    });
+
+// 受領通知完了ページ *ブロックへの書き込み
+app.post("/receipt", (req, res) => {
+    res.render(path.join(path.join(process.cwd(), viewspath), 'receipt_completed.ejs'),
+        {title: 'Receipt Page',
+         content: 'Completed!',
+         link:{href:'/', text: 'Back to top page!'}
+        });
+    /* network.receiptNotice(req.body.message).then((response) => {
+        res.send(response);}); */
+    }); 
+
+
+app.listen(3000, () => {
+    console.log('WebAPI Runnning');
+});
+
+
